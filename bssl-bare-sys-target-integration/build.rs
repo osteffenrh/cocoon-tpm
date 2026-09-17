776b4a4beb639b9e01e155f79826d536d08ab362
Oliver Steffen <osteffen@redhat.com>
2026-09-17 09:00:33 +0200

    bssl-bare-sys-target-integration: reformat
    Format fix by `cargo fmt`.
    
    Signed-off-by: Oliver Steffen <osteffen@redhat.com>

--- bssl-bare-sys-target-integration/build.rs
+++ bssl-bare-sys-target-integration/build.rs
@@ -3,7 +3,8 @@
 // Author: Nicolai Stange <nstange@suse.de>
 
 fn main() {
-    // See bssl-bare-sys' build.rs for a list and meaning of metadata variables recognized.
+    // See bssl-bare-sys' build.rs for a list and meaning of metadata variables
+    // recognized.
     // println!("cargo::metadata=CPPFLAGS=");
     // println!("cargo::metadata=BINDGEN_CFLAGS=");
     // println!("cargo::metadata=CFLAGS=");
